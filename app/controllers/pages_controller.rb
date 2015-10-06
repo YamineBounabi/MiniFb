17e15226d65d8a27e7226239935df09c5ea46c1c
adilbenseddik <adil.benseddik@gmail.com>
2015-10-06 10:30:28 +0200

    even more stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
   def index
     @walls = Wall.personal(current_user.id).includes(:user)
+    flash[:notice] = "#{@walls.length} Post(s)"
   end
 
   def show
     @user = User.find(params[:id])
+    flash[:notice] = "#{@user.relatives.length} Friend(s)"
   end
 
   def search
     @users = User.search(params[:query])
+    flash[:notice] = "#{@users.length} user(s)"
     render "results"
   end
 end
